170c2c8824e6bb7e361c8923b3f9710094fcd339
srikanth2209 <srikanthkareti7@gmail.com>
2021-10-02 07:14:50 +0530

    checking duplicate characters

---
+++ src/CheckingDuplicates.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+
+class CheckingDuplicates
+{
+    public static void countDuplicateCharacters(String str)
+    {
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        char[] charArray = str.toCharArray();
+        for (char c : charArray)
+        {
+            if (map.containsKey(c))
+            {
+                map.put(c, map.get(c) + 1);
+            }
+            else
+            {
+                map.put(c, 1);
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet())
+        {
+            if (entry.getValue() > 1)
+            {
+                System.out.println(entry.getKey() + " is repeted " + entry.getValue() + "times");
+            }
+        }
+    }
+    public static void main(String args[])
+    {
+        String str = "This is my first java code";
+        countDuplicateCharacters(str);
+    }
+}
+
+
